f7758da9340dd8026640977bdcad722fca780097
Nicholas Bauer <nicholasbauer@outlook.com>
2020-11-12 15:18:09 -0500

    Improved structure, caches data to avoid redownloading

--- src/weeklymap.jl
+++ src/weeklymap.jl
@@ -2,7 +2,7 @@ using Shapefile
 using Plots
 using XLSX
 
-function loadmapgeometry()
+function loadtowndata()
     path = joinpath("geodata", "TOWNSSURVEY_POLYM.shp")
     table = Shapefile.Table(path)
     sorted_order = sortperm(table.TOWN)
@@ -11,12 +11,15 @@ function loadmapgeometry()
     geoms, pop2010
 end
 
-function loadweekdata(datestring)
-    # cache data
-    path = download("https://www.mass.gov/doc/weekly-public-health-report-raw-data-$(datestring)-2020/download")
+function downloadweeklyreport(datestring)
+    path = joinpath("input","$(datestring).xlsx")
+    ispath(path) && return path
+    download("https://www.mass.gov/doc/weekly-public-health-report-raw-data-$(datestring)/download", path)
+end
+
+function loadweekdata(path)
     data = XLSX.readxlsx(path)
     sheet = XLSX.hassheet(data, "City_town") ? data["City_town"] : data["City_Town_Data"]
-    # sheet["C1"] == "Two Week Case Count" && sheet["D1"] == "Average Daily Incidence Rate per 100000"
     countsraw = sheet["C2:C352"]
     counts = [c == "<5" ? 2 : c for c ∈ countsraw] # replace "<5" with a number in range
     rates = sheet["D2:D352"]
@@ -24,66 +27,71 @@ function loadweekdata(datestring)
     return counts, rates, state_rate
 end
 
-function drawsavemap(datestring, categorycounts, pop2010)
-    counts, rates, state_rate = loadweekdata(datestring)
-
+function calculaterisklevels(counts, rates)
     risklevel = [r == 0 ? 0 :
-                c == 2 ? 1 :
-                r < 4 ? 2 :
-                r < 8 ? 3 :
-                r < 16 ? 4 :
-                r < 32 ? 5 :
-                r < 64 ? 6 : 7 for (c, r) ∈ zip(counts, rates)]
-    
-    riskcolors = Dict(0 => :gray95,
-                        1 => :gray85,
-                        2 => :limegreen,
-                        3 => :yellow,
-                        4 => :red,
-                        5 => :red3,
-                        6 => :darkred,
-                        7 => :black)
+                 c == 2 ? 1 :
+                 r < 4 ? 2 :
+                 r < 8 ? 3 :
+                 r < 16 ? 4 :
+                 r < 32 ? 5 :
+                 r < 64 ? 6 : 7 for (c, r) ∈ zip(counts, rates)]
+end
 
+geoms, pop2010 = loadtowndata()
+
+weeks = ["august-12-2020",
+         "august-19-2020",
+         "august-26-2020",
+         "september-2-2020",
+         "september-9-2020",
+         "september-16-2020",
+         "september-23-2020",
+         "september-30-2020",
+         "october-7-2020",
+         "october-14-2020",
+         "october-22-2020",
+         "october-29-2020",
+         "november-5-2020"]
+
+riskcolors = Dict(0 => :gray95,
+                  1 => :gray85,
+                  2 => :limegreen,
+                  3 => :yellow,
+                  4 => :red,
+                  5 => :red3,
+                  6 => :darkred,
+                  7 => :black)
+
+maps = []
+risklevelΔs = []
+categorycounts = []
+
+for w ∈ weeks
+    path = downloadweeklyreport(w)
+    counts, rates, state_rate = loadweekdata(path)
+    risklevel = calculaterisklevels(counts, rates)
+
+    colors = [riskcolors[r] for r ∈ risklevel] |> permutedims
+    push!(maps, plot(geoms, fillcolor=colors, linecolor=:gray75, linewidth=0.5, size=(1024,640), grid=false, showaxis=false, ticks=false, title=w))
+    savefig(joinpath("output", "$(w).png"))
+
+    # calculate weighted categories and append them
     weightedcategorycounts = AbstractFloat[]
     for k ∈ keys(sort(riskcolors))
         push!(weightedcategorycounts, sum(pop2010[dropdims(risklevel, dims=2) .== k]))
     end
     weightedcategorycounts = permutedims(weightedcategorycounts)
-    
-    if isempty(categorycounts)
-        categorycounts = weightedcategorycounts
-    else
-        categorycounts = [categorycounts; weightedcategorycounts]
-    end
-
-    colors = [riskcolors[r] for r ∈ risklevel] |> permutedims
-    plot(geoms, fillcolor=colors, linecolor=:gray75, linewidth=0.5, size=(1024,640), grid=false, showaxis=false, ticks=false, title=datestring)
-    areaplot!(categorycounts, fillcolor=permutedims(collect(values(sort(riskcolors)))), linewidth=0, legend=false, widen=false, inset=(1, bbox(0.1, 0.05, 0.4, 0.3, :bottom)), subplot=2, title="risk level by population")
-    savefig(joinpath("output", "$(datestring).png"))
-
-    return categorycounts
+    categorycounts = isempty(categorycounts) ? weightedcategorycounts : [categorycounts; weightedcategorycounts]
 end
 
-geoms, pop2010 = loadmapgeometry()
-
-files = ["august-12",
-         "august-19",
-         "august-26",
-         "september-2",
-         "september-9",
-         "september-16",
-         "september-23",
-         "september-30",
-         "october-7",
-         "october-14",
-         "october-22",
-         "october-29",
-         "november-5"]
-
+# State Animation
 anim = Plots.Animation()
-categorycounts = []
-for f ∈ files
-    categorycounts = drawsavemap(f, categorycounts, pop2010)
+for i ∈ eachindex(weeks)
+    plot(maps[i])
+    areaplot!(categorycounts[1:i,:], fillcolor=permutedims(collect(values(sort(riskcolors)))), linewidth=0, legend=false, widen=false,
+                     xaxis=((1,length(weeks)),30), xticks=(1:13,weeks),
+                     inset=(1, bbox(0.1, 0.1, 0.4, 0.3, :bottom)), subplot=2,
+                     title="risk level by population")
     Plots.frame(anim)
 end
 for i = 1:4 # insert 4 more of the same frame at end
