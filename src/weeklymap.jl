01d772104875e236525f787ffab9c262525a53cd
Nicholas Bauer <nicholasbauer@outlook.com>
2021-03-26 18:00:56 -0400

    percent positive maps

--- src/weeklymap.jl
+++ src/weeklymap.jl
@@ -30,6 +30,7 @@ function loadweekdata(path, date)
     if XLSX.hassheet(data, "Weekly_City_Town")
         sheet = data["Weekly_City_Town"]
         date_column = sheet["N1"] == "Report Date" ? "N" : "O" # they added a column
+        ppos_column = sheet["N1"] == "Report Date" ? "K" : "L" # they added a column
         dates = [zero(Date); Date.(filter(!ismissing, sheet[date_column][2:end]))] # first row is header, may be trailed by missing
         daterows = findall(x -> x == date, dates)[1:end]
         names = sheet["A"][daterows]
@@ -40,14 +41,16 @@ function loadweekdata(path, date)
 
         countsraw = sheet["E"][daterows]
         rates = sheet["F"][daterows]
+        ppos = sheet[ppos_column][daterows]
     else
         sheet = XLSX.hassheet(data, "City_town") ? data["City_town"] : data["City_Town_Data"]
         countsraw = sheet["C2:C352"]
         rates = sheet["D2:D352"]
+        ppos = sheet["I2:I352"]
     end
     counts = [c == "<5" ? 2 : c for c ∈ countsraw] # replace "<5" with a number in range
     
-    return counts, rates
+    return counts, rates, ppos
 end
 
 function calculaterisklevels(counts, rates)
@@ -59,7 +62,21 @@ function calculaterisklevels(counts, rates)
                  r < 32 ? 5 :
                  r < 64 ? 6 :
                  r < 128 ? 7 :
-                 r < 256 ? 8 : 9 for (c, r) ∈ zip(counts, rates)]
+                 r < 256 ? 8 :
+                 r < 512 ? 9 : 10 for (c, r) ∈ zip(counts, rates)]
+end
+
+function calculatepposrisklevels(counts, ppos)
+    pposrisklevel = [c == 0 ? 0 :
+                     p == 0 ? 1 :
+                     p < 0.002 ? 2 :
+                     p < 0.004 ? 3 :
+                     p < 0.008 ? 4 :
+                     p < 0.016 ? 5 :
+                     p < 0.032 ? 6 :
+                     p < 0.064 ? 7 :
+                     p < 0.128 ? 8 :
+                     p < 0.256 ? 9 : 10 for (c, p) ∈ zip(counts, ppos)]
 end
 
 geoms, pop2010 = loadtowndata()
@@ -95,7 +112,8 @@ weeks = ["august-12-2020",
          "february-25-2021",
          "march-4-2021",
          "march-11-2021",
-         "march-18-2021"]
+         "march-18-2021",
+         "march-25-2021"]
 
 labels = ["0 total",
           "<5 total",
@@ -108,6 +126,18 @@ labels = ["0 total",
           "128-256 /100k/day",
           "256-512 /100k/day",
           ">512 /100k/day"]
+
+pposlabels = ["0.0 %",
+              "~0.0 %",
+              "<0.2 %",
+              "0.2-0.4 %",
+              "0.4-0.8 %",
+              "0.8-1.6 %",
+              "1.6-3.2 %",
+              "3.2-6.4 %",
+              "6.4-12.8 %",
+              "12.8-25.6 %",
+              ">25.6 %"]
 
 riskcolors = Dict(0 => :gray95,
                   1 => :gray85,
@@ -123,19 +153,28 @@ riskcolors = Dict(0 => :gray95,
                   )
 
 ratemaps = []
+pposmaps = []
 categorycounts = []
+pposcategorycounts = []
 
 for w ∈ weeks
+    println(w)
     path = w ∈ weeks[1:22] ? downloadweeklyreport(w) :
                              downloadweeklyreport2(w)
     date = Date(w, DateFormat("U-d-y"))
-    counts, rates = loadweekdata(path, date)
+    counts, rates, ppos = loadweekdata(path, date)
     risklevel = calculaterisklevels(counts, rates)
     ndims(risklevel) == 1 || (risklevel = dropdims(risklevel, dims = 2))
 
     colors = [riskcolors[r] for r ∈ risklevel] |> permutedims
     push!(ratemaps, plot(geoms, fillcolor=colors, linecolor=:gray75, linewidth=0.5, size=(1024,640), grid=false, showaxis=false, ticks=false, title="Massachusetts COVID-19 Risk Level\n$(date)", labels=labels))
     savefig(joinpath("output", "$(w).png"))
+
+    pposrisklevel = calculatepposrisklevels(counts, ppos)
+    ndims(pposrisklevel) == 1 || (pposrisklevel = dropdims(pposrisklevel, dims = 2))
+    colors = [riskcolors[r] for r ∈ pposrisklevel] |> permutedims
+    push!(pposmaps, plot(geoms, fillcolor=colors, linecolor=:gray75, linewidth=0.5, size=(1024,640), grid=false, showaxis=false, ticks=false, title="Massachusetts COVID-19 Percent Positivity Risk Level\n$(date)", labels=labels))
+    savefig(joinpath("output", "$(w)-percent-positive.png"))
 
     # calculate weighted categories and append them
     weightedcategorycounts = AbstractFloat[]
@@ -144,6 +183,14 @@ for w ∈ weeks
     end
     weightedcategorycounts = permutedims(weightedcategorycounts)
     categorycounts = isempty(categorycounts) ? weightedcategorycounts : [categorycounts; weightedcategorycounts]
+
+    # calculate weighted categories and append them
+    pposweightedcategorycounts = AbstractFloat[]
+    for k ∈ keys(sort(riskcolors))
+        push!(pposweightedcategorycounts, sum(pop2010[pposrisklevel .== k]))
+    end
+    pposweightedcategorycounts = permutedims(pposweightedcategorycounts)
+    pposcategorycounts = isempty(pposcategorycounts) ? pposweightedcategorycounts : [pposcategorycounts; pposweightedcategorycounts]
 end
 
 dates = Date.(weeks, DateFormat("U-d-y"))
@@ -165,3 +212,20 @@ for i = 1:4 # insert 4 more of the same frame at end
 end
 gif(anim, joinpath("output", "animation_map.gif"), fps = 1)
 savefig(joinpath("output", "current_week_map.png"))
+
+anim = Plots.Animation()
+for i ∈ eachindex(weeks)
+    plot(pposratemaps[i])
+    areaplot!(pposcategorycounts[1:i,:], fillcolor=permutedims(collect(values(sort(riskcolors)))), linewidth=0, widen=false,
+                     xaxis=((1,length(weeks)),30), xticks=(1:2:length(dates), dates[1:2:end]),
+                     yaxis=("Population (millions)",), yformatter = x -> x / 1000000,
+                     tick_direction=:in,
+                     inset=(1, bbox(0.06, 0.1, 0.52, 0.3, :bottom)), subplot=2,
+                     legend=:outerright, labels=permutedims(labels))
+    Plots.frame(anim)
+end
+for i = 1:4 # insert 4 more of the same frame at end
+    Plots.frame(anim)
+end
+gif(anim, joinpath("output", "animation_map_percent_positivity.gif"), fps = 1)
+savefig(joinpath("output", "current_week_map_percent_positivity.png"))
