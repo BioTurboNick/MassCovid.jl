1c30353f26a78be3ae30ca8f9e71eb28a9b86053
Nicholas Bauer <nicholasbauer@outlook.com>
2021-06-17 22:21:00 -0400

    6-17 update

--- src/weeklymap.jl
+++ src/weeklymap.jl
@@ -126,7 +126,8 @@ weeks = ["august-12-2020",
          "may-20-2021",
          "may-27-2021",
          "june-3-2021",
-         "june-10-2021"]
+         "june-10-2021",
+         "june-17-2021"]
 
 labels = ["0 total",
           "<5 total",
